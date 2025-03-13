df4f2384758e2fcea5eee80452fc3b089d6dbc65
ForscherFreunde <forscherviking@gmail.com>
2025-03-13 14:34:19 +0100

    letzten changes - TestEntity raus

--- src/main/java/net/forscherfreunde/mod/TestMod.java
+++ src/main/java/net/forscherfreunde/mod/TestMod.java
@@ -6,7 +6,6 @@ import net.forscherfreunde.mod.entity.ModEntities;
 import net.forscherfreunde.mod.entity.custom.CloudyEntity;
 import net.forscherfreunde.mod.entity.custom.PorcupineEntity;
 import net.forscherfreunde.mod.entity.custom.SuperarmadilloEntity;
-import net.forscherfreunde.mod.entity.custom.TestEntity;
 import net.forscherfreunde.mod.registry.Mod;
 import net.forscherfreunde.mod.registry.item.ModItemGroups;
 import net.forscherfreunde.mod.registry.item.ModItems;
